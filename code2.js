c18fb562cb8fc2342bb38dc233ac1c403057b6b1
Ribe, Christian <Christian.Ribe@nuance.com>
2021-09-21 18:02:59 -0400

    main Added hidden iframe load

--- code2.js
+++ code2.js
@@ -17,9 +17,47 @@
 			
 		return pc1Enabled;
 	};
-
+	
+	function createHiddenIFrame (_id, _x, _y, _w, _h, src) {
+		try {
+			var obj = document.createElement('iframe');
+			obj.style.position = "absolute";
+			obj.style.width = _w ? _w : 0;
+			obj.style.height = _h ? _h : 0;
+			obj.style.left = _x ? _x + "px" : 0;
+			obj.style.top = _y ? _y + "px" : 0;
+			obj.style.zIndex = 99;
+			obj.style.display = "none";
+			obj.style.overflow = "hidden";
+			obj.style.padding = 0;
+			obj.style.margin = 0;
+			obj.id = _id;
+			obj.name = _id;
+			if (src) {
+				obj.src = src;
+			}
+			return obj;
+		} catch (E) {
+			this.debug("Could not create iFrame element.");
+		}
+		return null;
+	};
+	
 	var r = is1pcEnabled(true);
 	var el = document.getElementById("result");
 	el.innerHTML = "Write from code 2 == " + r;
+	
+	var iframe = createHiddenIFrame(666, 0, 0, 1, 1, "http://dud-page.pixagreat.com");
+	iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts');
+
+	if (window.addEventListener) {
+		iframe.addEventListener("load", function(){
+			console.log("IFrame loaded !!!!!");
+			console.log("IFrame src !!!!! =>", iframe.src);
+		}.bind(this), false);
+	}
+	document.getElementsByTagName("body")[0].appendChild(iframe);
+	
+	
 	
 })();
